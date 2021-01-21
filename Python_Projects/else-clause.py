064f9bdabf9db8e422e89f1c48e5b366cf55a725
heidi-walling <70841820+heidi-walling@users.noreply.github.com>
2021-01-21 14:55:22 +0200

    Create else-clause.py

---
+++ Python_Projects/else-clause.py
@@ -0,0 +1,12 @@
+#This is my else-clause
+
+import random
+import sys
+import os
+c = 3
+d = 4
+if c < d:
+...     print ("c is less than d")
+... else:
+...     print ("c is not less than d")
+... print ("outside the if block")
