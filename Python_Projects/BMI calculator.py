b74499d9d38ca6b0f64c05bfb0eedbbd4d4147c1
heidi-walling <70841820+heidi-walling@users.noreply.github.com>
2021-01-21 15:28:44 +0200

    Create BMI calculator.py

---
+++ Python_Projects/BMI calculator.py
@@ -0,0 +1,10 @@
+name = "Irma"
+height_m = 2
+weight_kg = 90
+bmi = weight_kg / (height_m * height_m)
+if bmi < 25:
+...     print (name)
+...     print ("is not overweight")
+... else:
+...     print (name)
+...     print ("is overweight")
